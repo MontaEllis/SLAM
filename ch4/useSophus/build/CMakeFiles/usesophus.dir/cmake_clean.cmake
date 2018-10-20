db8087cf5201cc302b34835099b326896dd506f5
MontaEllis <elliszkn@163.com>
2018-10-20 15:14:50 +0800

    second commit

---
+++ ch4/useSophus/build/CMakeFiles/usesophus.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/usesophus.dir/main.cpp.o"
+  "usesophus.pdb"
+  "usesophus"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/usesophus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
