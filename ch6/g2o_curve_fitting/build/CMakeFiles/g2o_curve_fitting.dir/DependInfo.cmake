51321fde543f390fc46f0b0f9ff5f364abec852a
MontaEllis <elliszkn@163.com>
2018-10-30 14:15:22 +0800

    4th commit

---
+++ ch6/g2o_curve_fitting/build/CMakeFiles/g2o_curve_fitting.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ellis/code/slam/ch6/g2o_curve_fitting/main.cpp" "/home/ellis/code/slam/ch6/g2o_curve_fitting/build/CMakeFiles/g2o_curve_fitting.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "OPENCV_TRAITS_ENABLE_DEPRECATED"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/eigen3"
+  "/usr/local/include"
+  "/usr/local/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ ch6/g2o_curve_fitting/build/CMakeFiles/g2o_curve_fitting.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/g2o_curve_fitting.dir/main.cpp.o"
+  "g2o_curve_fitting.pdb"
+  "g2o_curve_fitting"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/g2o_curve_fitting.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
