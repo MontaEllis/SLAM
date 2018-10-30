5b919f008dd677a494b178a43d2b96779443486e
MontaEllis <elliszkn@163.com>
2018-10-30 14:37:25 +0800

    4th commit

---
+++ ch3/ch3_useEigen/build/CMakeFiles/ch3_useeigen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ch3_useeigen.dir/main.cpp.o"
+  "ch3_useeigen.pdb"
+  "ch3_useeigen"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ch3_useeigen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ch3/useGeometry/build/CMakeFiles/usegeometry.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/usegeometry.dir/main.cpp.o"
+  "usegeometry.pdb"
+  "usegeometry"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/usegeometry.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
